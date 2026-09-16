8c136132917ee01756816c4c187ab664a59b4970
om magar <ommagar8806691515@gmail.com>
2026-09-16 19:15:19 +0530

    Fix CORS for Vercel frontend

--- ApiGateway/ApiGateway/src/main/java/com/omizoon/ApiGateway/config/CorsConfig.java
+++ ApiGateway/ApiGateway/src/main/java/com/omizoon/ApiGateway/config/CorsConfig.java
@@ -18,7 +18,10 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of(
+                        "http://localhost:5173",
+                        "https://cakeon-web-eight.vercel.app"
+                )
         );
 
         configuration.setAllowedMethods(
